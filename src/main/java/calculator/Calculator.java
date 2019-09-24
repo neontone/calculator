138e9459604d234f81a3ee9b007b637020da3b39
neontone <antons.korenevskis@gmail.com>
2019-09-24 20:25:16 +0300

    Simple calculations 3 numbers calculations

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,10 +8,23 @@ package calculator;
 public class Calculator {
 
     public String calculate(String[] expression) {
-        double a = Double.parseDouble(expression[0]);
-        String op = expression[1];
-        double b = Double.parseDouble(expression[2]);
+        String a = expression[0];
+        String op1 = expression[1];
+        String b = expression[2];
+        String op2 = expression[3];
+        String c = expression[4];
+
+        double result1 = calc(a, op1, b);
+        double result2 = calc(Double.toString(result1), op2, c);
+
+        return Double.toString(result2);
+    }
+
+    double calc(String stra, String op, String strb) {
+        double a = Double.parseDouble(stra);
+        double b = Double.parseDouble(strb);
         double result;
+
         switch (op) {
             case "+":
                 result = a + b;
@@ -29,7 +42,6 @@ public class Calculator {
                 result = 0;
         }
 
-        return Double.toString(result);
-
+        return result;
     }
 }
